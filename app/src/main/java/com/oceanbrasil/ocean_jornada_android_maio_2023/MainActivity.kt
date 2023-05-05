5a32b37ad8a7f875654de90649a1b90a69889532
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-05 09:57:28 -0300

    Desafio & bônus corrigidos

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_jornada_android_maio_2023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +19,26 @@ class MainActivity : AppCompatActivity() {
         // var -> variável que inicia o valor e pode mudar depois
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+        // Desafio: Pegar o que está no EditText e colocar no TextView
 
         // Criamos um Listener para o evento de clique no botão
         btEnviar.setOnClickListener {
             // O que escrevermos aqui nesse bloco
             // será executado ao clicar no botão
 
-            tvResultado.text = "Samsung Ocean"
+            if (etNome.text.isEmpty()) {
+                // Esse bloco só executa quando o etNome está vazio
+                // Portanto, preciso exibir o erro aqui
+                etNome.error = "Digite o nome!"
+            } else {
+                // Caso contrário, ou seja, se não estiver vazio, executa
+                // esse bloco
+                // Portanto, posso atualizar o TextView normalmente
+
+                tvResultado.text = etNome.text
+            }
         }
     }
 }
