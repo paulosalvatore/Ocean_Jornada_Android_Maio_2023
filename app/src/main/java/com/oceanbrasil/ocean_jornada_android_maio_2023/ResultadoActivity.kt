b1890c59c3de63e3782cc44b8411f6aa473d5444
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-05 11:29:54 -0300

    Botão voltar implementado

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/ResultadoActivity.kt
@@ -2,6 +2,7 @@ package com.oceanbrasil.ocean_jornada_android_maio_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
@@ -16,5 +17,11 @@ class ResultadoActivity : AppCompatActivity() {
         // recebido pela Intent
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         tvResultado.text = nomeDigitado
+
+        // Mecanismo do botão voltar
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
