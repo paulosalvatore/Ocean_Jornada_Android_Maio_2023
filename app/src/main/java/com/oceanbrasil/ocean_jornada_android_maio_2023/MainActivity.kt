319506b09c0696124f4fc63334b8b1fc5547d51c
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-10 10:29:09 -0300

    Mapa configurado

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.oceanbrasil.ocean_jornada_android_maio_2023
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openTreasureHuntActivity()
+    }
+
+    private fun openTreasureHuntActivity() {
+        val treasureHuntIntent = Intent(this, TreasureHuntActivity::class.java)
+        startActivity(treasureHuntIntent)
     }
 }
