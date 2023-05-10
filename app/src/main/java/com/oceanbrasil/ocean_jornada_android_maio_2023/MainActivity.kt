c1c74395e88465b8303c49f5b50a6cee729d2251
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-10 09:47:58 -0300

    Limpeza da aplicação
    - `ResultadoActivity` removida
    - Conteúdo da `MainActivity` removido

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/MainActivity.kt
@@ -1,63 +1,11 @@
 package com.oceanbrasil.ocean_jornada_android_maio_2023
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // <> -> Tipo da informação
-        // () -> ID do elemento
-        // Para acessar os IDs -> R.id.VALOR_DO_ID
-        // Variáveis -> val ou var
-        // val -> variável que inicia o valor e nunca mais muda
-        // var -> variável que inicia o valor e pode mudar depois
-        val tvResultado = findViewById<TextView>(R.id.tvResultado)
-        val btEnviar = findViewById<Button>(R.id.btEnviar)
-        val etNome = findViewById<EditText>(R.id.etNome)
-
-        // Desafio: Pegar o que está no EditText e colocar no TextView
-
-        // Criamos um Listener para o evento de clique no botão
-        btEnviar.setOnClickListener {
-            // O que escrevermos aqui nesse bloco
-            // será executado ao clicar no botão
-
-            if (etNome.text.isEmpty()) {
-                // Esse bloco só executa quando o etNome está vazio
-                // Portanto, preciso exibir o erro aqui
-                etNome.error = "Digite o nome!"
-            } else {
-                // Caso contrário, ou seja, se não estiver vazio, executa
-                // esse bloco
-                // Portanto, posso atualizar o TextView normalmente
-
-                tvResultado.text = etNome.text
-            }
-        }
-
-        // Abrir Nova Tela
-        val btAbrirNovaTela = findViewById<Button>(R.id.btAbrirNovaTela)
-
-        btAbrirNovaTela.setOnClickListener {
-            // Criar uma Intent para abrir a nova tela
-            val abrirNovaTelaIntent = Intent(this, ResultadoActivity::class.java)
-
-            // Pegamos a informação digitada no etNome, que vem como CharSequence
-            // Convertemos para String, para pegá-la como String lá na outra tela
-            val nomeDigitado = etNome.text.toString()
-
-            // Inserimos as informações novas na Intent
-            abrirNovaTelaIntent.putExtra("NOME_DIGITADO", nomeDigitado)
-
-            // Registra a Intent no Android
-            startActivity(abrirNovaTelaIntent)
-        }
     }
 }
