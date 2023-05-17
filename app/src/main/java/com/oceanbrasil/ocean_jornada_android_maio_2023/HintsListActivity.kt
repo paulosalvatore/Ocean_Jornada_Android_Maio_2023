ac348f1cb1646ac8c70d52b3951b9e418e637639
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-17 11:31:34 -0300

    Inicialização da lista e criação do `Adapter` (WIP)

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/HintsListActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/HintsListActivity.kt
@@ -2,10 +2,29 @@ package com.oceanbrasil.ocean_jornada_android_maio_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HintsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hints_list)
+
+        // Pegar a referência da RecyclerView
+        val rvHints = findViewById<RecyclerView>(R.id.rvHints)
+
+        // Inicializar LayoutManager
+        val layoutManager = LinearLayoutManager(this)
+        rvHints.layoutManager = layoutManager
+
+        // Inicializar Adapter (Precisamos criar o Adapter)
+        // TODO: Substituir listOf pela chamada na API
+        val hintsList = listOf(
+            Hint(1, "Local da Dica", 10.0, 10.0),
+            Hint(2, "Local da Dica", 10.0, 10.0),
+            Hint(3, "Local da Dica", 10.0, 10.0)
+        )
+        val adapter = HintsListAdapter(hintsList)
+        rvHints.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/HintsListAdapter.kt
@@ -0,0 +1,21 @@
+package com.oceanbrasil.ocean_jornada_android_maio_2023
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HintsListAdapter(items: List<Hint>) : RecyclerView.Adapter<HintsListAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
