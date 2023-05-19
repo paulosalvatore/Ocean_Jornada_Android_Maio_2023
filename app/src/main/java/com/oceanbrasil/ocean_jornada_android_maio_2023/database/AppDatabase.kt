024fbe62b75e1fd86da5d58c44277437664ff3c4
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-19 11:26:13 -0300

    `AppDatabase` implementado

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/database/AppDatabase.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/database/AppDatabase.kt
@@ -1,4 +1,23 @@
 package com.oceanbrasil.ocean_jornada_android_maio_2023.database
 
-abstract class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [HintEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun hintDao(): HintDao
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "app_database"
+            ).build().also { instance = it }
+        }
+    }
 }
