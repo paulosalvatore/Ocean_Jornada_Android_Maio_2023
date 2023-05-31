05e6a2afb957c6fe9a8491ceb2b13a3896640a17
Paulo Salvatore <salvatoregames@gmail.com>
2023-05-31 10:51:44 -0300

    Inicialização dos dados ao criar DB

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/view/hints/HintsListActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_maio_2023/view/hints/HintsListActivity.kt
@@ -15,23 +15,12 @@ class HintsListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hints_list)
 
         // TODO: Extrair
-        val hintEntity = HintEntity(1, "Brigadeiro", 10.0, 70.0)
-        val hintEntity2 = HintEntity(2, "Trianon Masp", 20.0, 60.0)
-        val hintEntity3 = HintEntity(3, "Paraíso", 30.0, 50.0)
-        val hintEntity4 = HintEntity(4, "Consolação", 40.0, 40.0)
-
-        // TODO: Extrair
         val appDatabase = AppDatabase.getInstance(this)
         val hintDao = appDatabase.hintDao()
 
         // Iniciamos uma Thread nova
         Thread {
             // Executamos operações fora da Main Thread
-//            hintDao.insert(hintEntity)
-//            hintDao.insert(hintEntity2)
-//            hintDao.insert(hintEntity3)
-//            hintDao.insert(hintEntity4)
-
             // TODO: Extrair
             val hintsEntities = hintDao.findAll()
 
@@ -44,11 +33,6 @@ class HintsListActivity : AppCompatActivity() {
                 rvHints.layoutManager = layoutManager
 
                 // Inicializar Adapter (Precisamos criar o Adapter)
-//                val hintsList = listOf(
-//                    Hint(1, "Brigadeiro", 10.0, 70.0),
-//                    Hint(2, "Trianon Masp", 20.0, 60.0),
-//                    Hint(3, "Paraíso", 30.0, 40.0)
-//                )
                 // TODO: Precisamos já receber o dado de Domain
                 val hintsList = hintsEntities.map {
                     HintApiModel(it.id, it.name, it.latitude, it.longitude)
